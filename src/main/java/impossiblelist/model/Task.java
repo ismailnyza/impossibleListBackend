f5a16e53f50f0d0cbd4c7172a2462833bebb8fc1
ismail nyza <ismailnyzarsl@gmail.com>
2024-08-13 23:42:20 +0530

    READ MEEE

--- src/main/java/impossiblelist/model/Task.java
+++ src/main/java/impossiblelist/model/Task.java
@@ -5,23 +5,35 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 @Document(collection = "tasks")
 public class Task {
     private String id;
-
     private String name;
     private String description;
     private Date dueDate;
-//    how do only enable specific features
     private priority priority;
     private status status;
     private List<tag> tagList;
-//    todo add subtasks
+    private List<Task> subTaskList;
     private List<comment> commentList;
     private User assignee;
     private User creator;
     private Date createdDate;
-    private Date lastUpdatedDate;;
+    private Date lastUpdatedDate;
+
+    public Task(User assignee, List<comment> commentList, Date createdDate, User creator, String description, Date dueDate, Date lastUpdatedDate, impossiblelist.model.priority priority, String name, impossiblelist.model.status status, List<tag> tagList, List<Task> subTaskList) {
+        this.commentList = commentList;
+        this.createdDate = createdDate;
+        this.creator = creator;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.lastUpdatedDate = createdDate;
+        this.priority = Objects.requireNonNullElseGet(priority, () -> impossiblelist.model.priority.valueOf("LOW"));
+        this.name = name;
+        this.status = Objects.requireNonNullElseGet(status, () -> impossiblelist.model.status.valueOf("PENDING"));
+        this.tagList.addAll(tagList);
+        }
 }
--- src/main/java/impossiblelist/model/tag.java
+++ src/main/java/impossiblelist/model/tag.java
@@ -1,8 +1,24 @@
 package impossiblelist.model;
 
+import lombok.Data;
+
+@Data
 public class tag {
     private String id;
     private String name;
     private String description;
     private String color;
+
+    public tag(String name, String description, String color) {
+        this.name = name;
+        this.description = description;
+        this.color = color;
+    }
+
+    public tag(String id, String name, String description, String color) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.color = color;
+    }
 }
